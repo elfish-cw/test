e029b32c3e381bc203a66f708b696320d30cce98
Lana <elfish_cw@mail.ru>
2023-03-30 11:47:35 +0300

    решила задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите символьный массив, разделяя его пробелами: ");
+string? text = Console.ReadLine();
+string[] ChooseThreeCharWords(string s)
+{
+    string[] subs = s.Split(' ');
+    string resultstring = "";
+    for (int i = 0; i < subs.Length; i++)
+    {
+        if (subs[i].Length <= 3)
+        {
+            resultstring = resultstring.Insert(0, $" {subs[i]}");
+        }
+    }
+    string[] result = resultstring.Split(' ');
+    return result;
+}
+
+
+string[] resulttext = ChooseThreeCharWords(text);
+Console.Write($"Искомый массив:");
+for (int j = 0; j < resulttext.Length; j++)
+{
+    Console.Write($" {resulttext[j]}");
+}
+Console.WriteLine();
+
+
